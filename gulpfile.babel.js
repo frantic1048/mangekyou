64cd206c6fe473a1daf425e07b57b182cfc6f85c
frantic1048 <archer@frantic1048.com>
2015-10-27 22:35:46 +0800

    v0.0.3:let's start...

---
+++ gulpfile.babel.js
@@ -0,0 +1,95 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import sass from 'gulp-sass';
+import newer from 'gulp-newer';
+import sourcemaps from 'gulp-sourcemaps';
+import eslint from 'gulp-eslint';
+
+
+const app = {
+  js: {},
+  css: {},
+  html: {},
+};
+
+const vendor = {
+  js: {},
+  css: {},
+  font: {},
+};
+
+app.js.src = [
+  'src/app/main.js',
+  'src/app/**/*.+(js|jsx)',
+];
+app.js.dest = [
+  'build/app/main.js',
+  'build/app/**/*.js',
+];
+app.js.destPath = 'build/app';
+app.js.lintSrc = [
+  app.js.src,
+  'gulpfile.babel.js',
+];
+
+app.css.src = 'src/app/style/*.+(css|scss)';
+app.css.srcCompiled = 'src/app/style/*.css';
+app.css.dest = 'build/app/style/app.css';
+app.css.destPath = 'build/app/style';
+
+app.html.src = 'src/app/index.html';
+app.html.destPath = 'build/app';
+
+vendor.font.src = [
+  'bower_components/material-design-icons/iconfont/*.+(eot|ttf|woff|woff2)',
+];
+vendor.font.destPath = 'build/app/font';
+
+gulp.task('vendor-font', () => {
+  return gulp.src(vendor.font.src)
+    .pipe(gulp.dest(vendor.font.destPath));
+});
+
+gulp.task('vendor', gulp.series(
+  'vendor-font'
+));
+
+gulp.task('html', () => {
+  return gulp.src(app.html.src)
+    .pipe(gulp.dest(app.html.destPath));
+});
+
+gulp.task('lint', () => {
+  return gulp.src(app.js.lintSrc)
+    .pipe(eslint({ rulePaths: ['./']} ))
+    .pipe(eslint(eslint.format()));
+});
+
+gulp.task('css', () => {
+  return gulp.src(app.css.src)
+    .pipe(newer(app.css.destPath))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(app.css.destPath));
+});
+
+gulp.task('js', () => {
+  return gulp.src(app.js.src)
+    .pipe(newer(app.js.destPath))
+    .pipe(sourcemaps.init())
+    .pipe(babel({ modules: 'common' }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(app.js.destPath));
+});
+
+gulp.task('dev', (callback) => {
+  gulp.watch(app.html.src, 'html');
+  gulp.watch(app.css.src, 'css');
+  gulp.watch(app.js.src, 'css');
+  callback()
+});
+
+gulp.task('default', (callback) => {
+  callback();
+});
---
+++ src/app/main.js
@@ -0,0 +1,60 @@
+import app from 'app';
+import BrowserWindow from 'browser-window';
+import ipc from 'ipc';
+
+require('crash-reporter').start();
+
+// for dev
+// auto reload when file changes
+require('electron-reload')(__dirname, {
+  electron: require('electron-prebuilt')
+});
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+  const mangekyouWindow = {
+    width: 800,
+    height: 600,
+    'min-width': 800,
+    'min-height': 600,
+    frame: false,
+    show: false,
+  };
+
+  mainWindow = new BrowserWindow(mangekyouWindow);
+
+  // for product
+  // disable default electron menu
+  // mainWindow.setMenu(null);
+
+  mainWindow.loadUrl(`file://${__dirname}/index.html`);
+  mainWindow.show();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  ipc.on('Wminimize', () => {
+    mainWindow.minimize();
+  });
+  ipc.on('Wmaximize', () => {
+    if (mainWindow.isMaximized()) {
+      mainWindow.unmaximize();
+    } else {
+      mainWindow.maximize();
+    }
+  });
+  ipc.on('Wclose', () => {
+    mainWindow.close();
+  });
+  ipc.on('Wreload', () => {
+    mainWindow.reload();
+  });
+});
