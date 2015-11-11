552a5f3148946c51265e76ebb1e8e26b28913ca5
frantic1048 <archer@frantic1048.com>
2015-11-11 21:23:34 +0800

    v0.1.15:start building new history list

--- src/app/script/component/History.jsx
+++ src/app/script/component/History.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import List from 'material-ui/lib/lists/list';
+import HistoryItem from './HistoryItem';
+import mangekyouStore from './../store/mangekyouStore';
+import mangekyouAction from './../action/mangekyouAction';
 
 // TODO: History component
 const History = React.createClass({
+  getInitialState() {
+    return {
+      historyList: mangekyouStore.getHistory(),
+    };
+  },
+  componentDidMount() {
+    mangekyouStore.addHistoryChangeListener(this._onChange);
+  },
+  componentWillUnmount() {
+    mangekyouStore.removeHistoryChangeListener(this._onChange);
+  },
   render() {
+    const listItems = [];
+    for (const history of this.state.historyList) {
+      listItems.push(
+        <HistoryItem
+          key={history.key}
+          history={history}
+        />
+      );
+    }
     return ( // eslint-disable-line no-extra-parens
       <h2>History Panel</h2>
     );
+  },
+  _onChange() {
+    this.setState({
+      historyList: mangekyouStore.getHistory,
+    });
   },
 });
 
---
+++ src/app/script/component/HistoryItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Avatar from 'material-ui/lib/avatar';
+import IconButton from 'material-ui/lib/icon-button';
+import ActionHistory from 'material-ui/lib/svg-icons/action/history';
+
+const HistoryItem = React.createClass({
+  render() {
+    return <h1>HistoryItem</h1>;
+  },
+});
+
+export default HistoryItem;
